d8defc37ad5cadda17e445c71f7de38cf9d242c5
Osman Saad Osman <osman.saad7121@gmail.com>
2024-08-05 18:05:40 -0700

    +Task1: Calculator UI Linear Layout +Task2: Relative Layout Task +Task3: Register Screen Ui Constraint Layout

---
+++ app/src/main/java/com/pbws/android_assignment_1_2/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.pbws.android_assignment_1_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+
+    }
+}
---
+++ app/src/main/java/com/pbws/android_assignment_1_2/RelativeLayoutActivity.kt
@@ -0,0 +1,15 @@
+package com.pbws.android_assignment_1_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RelativeLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+
+    }
+}
